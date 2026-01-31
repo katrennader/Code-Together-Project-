1976564ae07cf9b711f77a52de70485007eee981
katren nader <katrennader16@gmail.com>
2026-01-31 20:30:06 +0200

    fix redis issue

--- queues/codeQueue.js
+++ queues/codeQueue.js
@@ -1,11 +1,22 @@
 //  connect bull with redis 
+require("dotenv").config();
 
 const Queue = require("bull");
+const Redis = require("ioredis");
+
 const codeQueue = new Queue("code-queue", {
   redis: {
-    host: "127.0.0.1",
-    port: 6379
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT
   }
+});
+
+redis.on("connect", () => {
+    console.log("Connected to Redis successfully");
+});
+
+redis.on("error", (err) => {
+    console.error("Redis error:", err);
 });
 
 module.exports = codeQueue;
